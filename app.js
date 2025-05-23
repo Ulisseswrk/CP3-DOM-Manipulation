8738485516f97004d1d96285081431bbd2ea288c
Arthur <tuturley@gmail.com>
2025-05-23 01:56:49 -0300

    add: Função para mostrar os produtos

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //Array dos produtos:
-let produtos = [{nome: "Camisa", preco: 49.9, categoria: "Superior", disponibilidade: false},
+let produtos = [
+    {nome: "Camisa", preco: 49.9, categoria: "Superior", disponibilidade: false},
     {nome: "Regata", preco: 20, categoria: "Superior", disponibilidade: true},
     {nome: "Camiseta", preco: 25, categoria: "Superior", disponibilidade: true},
     {nome: "Cueca", preco: 15, categoria: "Roupas Intimas", disponibilidade: true},
@@ -9,4 +10,37 @@ let produtos = [{nome: "Camisa", preco: 49.9, categoria: "Superior", disponibili
     {nome: "Chinelo", preco: 20, categoria: "Calçados", disponibilidade: false},
     {nome: "Percata", preco: 500, categoria: "Calçados", disponibilidade: true},
     {nome: "Calsola", preco: 10, categoria: "Roupas Intimas", disponibilidade: false},
-]
+];
+
+//Constantes
+let container = document.getElementById("containerProdutos");
+let botaoListar = document.getElementById("btnListar");
+
+//deixa limpo antes de mostrar
+botaoListar.addEventListener('click', function(){
+    container.innerHTML = "";
+
+
+    produtos.forEach(function(produtos) {
+        let div = document.createElement("div");
+        div.classList.add("produto");
+        let nome = document.createElement("h3");
+        nome.textContent = produtos.nome;
+        div.appendChild(nome);
+        let preco = document.createElement("p");
+        preco.textContent = "Preço: R$ " + produtos.preco.toFixed(2);
+        div.appendChild(preco);
+        let categoria = document.createElement("p");
+        categoria.textContent = "Categoria: " + produtos.categoria;
+        div.appendChild(categoria);
+        let disponibilidade = document.createElement("p");
+        if (produtos.disponibilidade) {
+            disponibilidade.textContent = "Disponível";
+        }
+        else {
+            disponibilidade.textContent = "Indisponível";
+        }
+        div.appendChild(disponibilidade);
+        container.appendChild(div);
+    });
+});
