ab51fa28da142a2b3c19d2d85e86065cb7aec07d
Arthur <tuturley@gmail.com>
2025-05-23 01:10:40 -0300

    add: Adicionar arrays dos produtos

---
+++ app.js
@@ -0,0 +1,12 @@
+//Array dos produtos:
+let produtos = [{nome: "Camisa", preco: 49.9, categoria: "Superior", disponibilidade: false},
+    {nome: "Regata", preco: 20, categoria: "Superior", disponibilidade: true},
+    {nome: "Camiseta", preco: 25, categoria: "Superior", disponibilidade: true},
+    {nome: "Cueca", preco: 15, categoria: "Roupas Intimas", disponibilidade: true},
+    {nome: "Calcinha", preco: 15, categoria: "Roupas Intimas", disponibilidade: false},
+    {nome: "Meia Calça", preco: 10, categoria: "Roupas Intimas", disponibilidade: true},
+    {nome: "Tênis", preco: 120, categoria: "Calçados", disponibilidade: true},
+    {nome: "Chinelo", preco: 20, categoria: "Calçados", disponibilidade: false},
+    {nome: "Percata", preco: 500, categoria: "Calçados", disponibilidade: true},
+    {nome: "Calsola", preco: 10, categoria: "Roupas Intimas", disponibilidade: false},
+]
